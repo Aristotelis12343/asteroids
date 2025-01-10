3a25e10c019ce3119782ab877630ce89661ad566
Aristotelis12343 <ar1stotel1stsiol@gmail.com>
2025-01-10 23:04:25 +0200

    added the circleshape.py file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 
 pygame.init
+clock = pygame.time.Clock()
+dt = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 def main():
@@ -14,7 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        dt= clock.tick(60) / 1000
+        
 if __name__=="__main__":
     main()
 
