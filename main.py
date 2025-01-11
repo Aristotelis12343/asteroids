43ca7a18868483997066d7802544e0710e269cf2
Aristotelis12343 <ar1stotel1stsiol@gmail.com>
2025-01-11 21:38:06 +0200

    added rotation for a and d buttons for triangle

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 from constants import *
 from player import Player
 
-pygame.init
+pygame.init()
 clock = pygame.time.Clock()
-dt = 0
+
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 def main():
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,6 +17,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt= clock.tick(60) / 1000
