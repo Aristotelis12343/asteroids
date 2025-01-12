7d7d2bf32107d161fb6dfc66f668d228aa32e741
Aristotelis12343 <ar1stotel1stsiol@gmail.com>
2025-01-13 00:08:11 +0200

    added shooting

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -30,6 +31,7 @@ class Player(CircleShape):
     def shoot(self):
         shot = Shot(self.position.x,self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -43,4 +45,7 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer<=0:
+                self.shoot()
+            else:
+                self.timer-=dt
